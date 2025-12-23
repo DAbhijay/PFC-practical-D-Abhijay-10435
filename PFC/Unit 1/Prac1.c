b68fda9cca147727e4e5fc8e1e450ff6558fcd8e
D Abhijay <rexyonfire0808@gmail.com>
2025-12-24 01:42:58 +0530

    first commit

---
+++ PFC/Unit 1/Prac1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Abhijay";
+    int age = 25;
+    int ERP = 10435;
+    
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("ERP: %d\n", ERP);
+
+    return 0;
+}
